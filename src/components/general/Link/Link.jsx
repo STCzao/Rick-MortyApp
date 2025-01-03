da67e2f13769ccd92a4aeb49ec82758cf592b392
STCzao <Santicoronel0183@gmail.com>
2025-01-03 18:11:27 -0300

    Creacion del navbar

---
+++ src/components/general/Link/Link.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Link = ({title}) => {
+  return (
+    <li className="nav-item">
+      <a className="nav-link active" aria-current="page" href="#">
+        {title} 
+      </a>
+    </li>
+  );
+};
+
+export default Link;
