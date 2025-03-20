f4b9c4c3d3de39a293888ef14133fbe7ff89a8dd
STCzao <Santicoronel0183@gmail.com>
2025-03-20 17:15:40 -0300

    Se trabajo en la busqueda por nombre

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/general/Navbar";
 import Home from "./pages/Home";
 import CharacterDetail from "./pages/CharacterDetail";
+import React, { useState } from "react";
 
 const App = () => {
+
+  const [characterName, setCharacterName] = useState("");
+
   return (
-    //El App se va a encargar solo de las rutas en este caso. Vamos a navegar entre componentes sin salir de la page.
-    <BrowserRouter>
-      <Navbar />
+    //El App se va a encargar solo de las rutas en este caso. Vamos a navegar entre componentes sin salir de la page
+    //Utilizamos el hook de estado para setCharacterName y cada vez que se cambia el valor en el componente Navbar, lo guardamos aca
+    //Pasamos la prop a Home, la desestructuramos y la agregamos a la peticion Http que se va a ejecutar cuando el useEffect monte el componente
+    <BrowserRouter> 
+      <Navbar setCharacterName={setCharacterName}/> 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home characterName={characterName}/>} />
         <Route path="/character/:id" element={<CharacterDetail />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,14 +5,14 @@ import { useState } from "react";
 import ReactPaginate from "react-paginate";
 import Loader from "../components/general/Loader/Loader";
 
-const Home = () => {
-  const [characters, setCharacters] = useState([]);
-  const [totalPages, setTotalPages] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
+const Home = ({ characterName }) => {
+  const [characters, setCharacters] = useState([]);//Lo utilizo para guardar las caracteristicas de results
+  const [totalPages, setTotalPages] = useState(0); //Lo utilizo para guardar las paginas
+  const [currentPage, setCurrentPage] = useState(1); //Lo utilizo para contabilizar las paginas y guardar aca el resultado
+  const [isLoading, setIsLoading] = useState(false);//Lo utilizo para cargar las cards segun la interaccion del usuario
 
   const handleRequest = async () => {
-    setIsLoading;
+    setIsLoading(true);
     try {
       const {
         data: {
@@ -20,25 +20,25 @@ const Home = () => {
           info: { pages },
         },
       } = await Axios(
-        `https://rickandmortyapi.com/api/character?page=${currentPage}`
-      ); //Desestructuro "data" para poder acceder a sus propiedades
-      setCharacters(results); //Cuando la pagina cambie su valor, ingresa al estado y compara el virtual DOM viejo con el nuevo. Si encuentra alguna diferencia, la cambia y renderiza
+        `https://rickandmortyapi.com/api/character?page=${currentPage}&name=${characterName}`); 
+      setCharacters(results); 
       setTotalPages(pages);
     } catch (error) {
       console.log(error);
     } finally {
       setIsLoading(false);
     }
-  };
+};
+
 
   useEffect(() => {
-    handleRequest(); //Se ejecuta una vez que el componente esta cargado
-  }, [currentPage]); //Cuando current page cambia el valor, se ejecuta y actualiza el handle request
+    handleRequest(); //Se ejecuta y monta el componente cuando esta cargado
+  }, [currentPage, characterName]); //Cuando currentpage/charactername cambia el valor, se ejecuta y actualiza el handle request
 
   const createCard = () =>
     characters.map((character) => (
       <CharacterCards key={character.id} character={character} col={3} />
-    ));
+    )); //El map ingresa al string de characters. Luego, cada card es una posicion del string y esta determinado por un Id, para traer sus caracteristicas, hay que pasarle una key que busque ID dentro de Character
 
   const handlePageClick = (selectedPage) => setCurrentPage(selectedPage + 1);
 
@@ -46,8 +46,8 @@ const Home = () => {
     <>
       <div className="container-fluid pt-5">
         <div className="row justify-content-center gap-5">
-          {isLoading ? <Loader /> : createCard()};
-        </div>
+          {isLoading ? <Loader /> : createCard()}; 
+        </div> 
         <ReactPaginate //Estilos y funciones del react-paginate
           pageCount={totalPages}
           onPageChange={(data) => handlePageClick(data.selected)}
