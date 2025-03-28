09fe46ffd728d41e30a5de4376d253d05f2d1b44
STCzao <Santicoronel0183@gmail.com>
2025-03-28 13:49:07 -0300

    Se refinaron detalles del buscador y las Cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,10 +6,10 @@ import ReactPaginate from "react-paginate";
 import Loader from "../components/general/Loader/Loader";
 
 const Home = ({ characterName }) => {
-  const [characters, setCharacters] = useState([]);//Lo utilizo para guardar las caracteristicas de results
+  const [characters, setCharacters] = useState([]); //Lo utilizo para guardar las caracteristicas de results
   const [totalPages, setTotalPages] = useState(0); //Lo utilizo para guardar las paginas
   const [currentPage, setCurrentPage] = useState(1); //Lo utilizo para contabilizar las paginas y guardar aca el resultado
-  const [isLoading, setIsLoading] = useState(false);//Lo utilizo para cargar las cards segun la interaccion del usuario
+  const [isLoading, setIsLoading] = useState(false); //Lo utilizo para cargar las cards segun la interaccion del usuario
 
   const handleRequest = async () => {
     try {
@@ -19,43 +19,46 @@ const Home = ({ characterName }) => {
           info: { pages },
         },
       } = await Axios(
-        `https://rickandmortyapi.com/api/character?page=${currentPage}&name=${characterName}`); 
-      setCharacters(results); 
+        `https://rickandmortyapi.com/api/character?page=${currentPage}&name=${characterName}`
+      );
+      setCharacters(results);
       setTotalPages(pages);
-    } catch (error) {
-      console.log(error);
+    } catch {
+      setCharacters([]);
+      setTotalPages(0);
     } finally {
       setIsLoading(false);
     }
-};
-
+  };
 
   useEffect(() => {
-
     setIsLoading(true); //Cuando se monte el useEffect, setIsLoading se coloca en true y el SetTimeOut ejecuta el HandleRequest despues de cierto tiempo
 
-    const searchInterval = setTimeout(() => { 
+    const searchInterval = setTimeout(() => {
       handleRequest(); //Se ejecuta y monta el componente cuando esta cargado
-    },1000);
+    }, 1000);
 
     return () => {
       clearInterval(searchInterval); //Una vez que dejo de escribir caracteres, limpiamos el intervalo de SetTimeOut para que el HandleRequest se ejecute
-    }
+    };
   }, [currentPage, characterName]); //Cuando currentpage/charactername cambia el valor, se ejecuta y actualiza el handle request
-
-  const createCard = () =>
-    characters.map((character) => (
-      <CharacterCards key={character.id} character={character} col={3} />
-    )); //El map ingresa al string de characters. Luego, cada card es una posicion del string y esta determinado por un Id, para traer sus caracteristicas, hay que pasarle una key que busque ID dentro de Character
-
+ 
+  const createCard = () => {
+    return characters.length > 0 ? characters.map ((character) => <CharacterCards key={character.id} character={character} col={3}/>) : <h1 className="text-center"> No se encontraron resultados para su busqueda</h1>
+  };
+    //El map ingresa al string de characters. Luego, cada card es una posicion del string y esta determinado por un Id, para traer sus caracteristicas hay que pasarle una key que busque ID dentro de Character
+    //Si hay caracteres por leer, se ejecuta el characters map. Si no encuentra resultado, devuelve el H1
   const handlePageClick = (selectedPage) => setCurrentPage(selectedPage + 1);
 
   return (
     <>
       <div className="container-fluid pt-5">
-        <div className="row justify-content-center gap-5">
-          {isLoading ? <Loader /> : createCard()}; 
-        </div> 
+        <div
+          className="row justify-content-center gap-5"
+          style={{ minHeight: "70vh" }}
+        >
+          {isLoading ? <Loader /> : createCard()}
+        </div>
         <ReactPaginate //Estilos y funciones del react-paginate
           pageCount={totalPages}
           onPageChange={(data) => handlePageClick(data.selected)}
